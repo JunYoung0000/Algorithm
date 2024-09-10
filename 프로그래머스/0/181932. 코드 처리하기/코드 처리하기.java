a2de9a1a5c8104a88a28c28e2cd536d84f9f3680
JunYoung0000 <141308805+JunYoung0000@users.noreply.github.com>
2024-09-10 23:50:08 +0900

    [level 0] Title: 코드 처리하기, Time: 8.53 ms, Memory: 91.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(String code) {
+        StringBuilder ret = new StringBuilder();
+        int mode = 0;
+
+        for (int i =0; i< code.length(); i++){
+            if(mode == 0){
+                if(code.charAt(i) == '1')
+                    mode = 1;
+                else
+                    if(i % 2== 0)
+                        ret.append(code.charAt(i));
+            }
+            else if(mode == 1){
+                if(code.charAt(i) == '1')
+                    mode = 0;
+                else
+                    if(i%2==1)
+                        ret.append(code.charAt(i));
+            }
+        }
+        return ret.length() == 0 ? "EMPTY" : ret.toString();
+    }
+}
