d972620e4cb1e4aa2290e49b1102572695e7612d
JunYoung0000 <141308805+JunYoung0000@users.noreply.github.com>
2024-09-11 15:17:27 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.06 ms, Memory: 66.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] num_list) {
+    int answer = 0;
+    int num1 = 1;
+    int num2 = 0;
+    
+    for(int i =0; i < num_list.length; i++){
+        num1 *= num_list[i];
+        num2 += num_list[i];
+    }
+    num2 = (int)Math.pow(num2, 2);
+    
+    if(num1 < num2)
+        answer = 1;
+    
+    return answer;
+    }
+}
