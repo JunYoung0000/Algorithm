9c7b7b9a9f945742710bd610aae44ce52cf718ca
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2025-02-12 21:07:47 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.14 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(my_string.substring(0, s));
+        for(int i = e; i >= s; i--){
+            sb.append(my_string.charAt(i));
+        }
+        if(e +1 < my_string.length())
+            sb.append(my_string.substring(e+1));
+        
+        return sb.toString();
+    }
+}
