4069bb320db43ea6342c9da3054556208b31889c
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2025-03-03 18:06:58 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String[] solution(String[] names) {
+        int size = names.length / 5;
+        if(names.length % 5 != 0){
+            size ++;
+        }
+        
+        String[] answer = new String[size];
+        
+        for(int i = 0; i<size; i++){
+            answer[i] = names[i*5];
+        }
+        
+        return answer;
+    }
+}
