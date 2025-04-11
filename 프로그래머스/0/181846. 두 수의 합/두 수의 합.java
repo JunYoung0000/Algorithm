3287e2eb0708c2ca1365ff58d11158d65bb759e2
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2025-04-11 14:46:32 +0900

    [level 0] Title: 두 수의 합, Time: 23.72 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int digitA = (i >= 0) ? a.charAt(i--) - '0' : 0;
+            int digitB = (j >= 0) ? b.charAt(j--) - '0' : 0;
+
+            int sum = digitA + digitB + carry;
+            result.append(sum % 10);
+            carry = sum / 10;
+        }
+
+        return result.reverse().toString();
+    }
+}
