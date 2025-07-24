1ed82b9597e54852ce6e342e29ad63174cab5577
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2025-07-24 16:14:41 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.07 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        for(int i = 1; i <= n; i=i+2){
+            arr.add(i);
+        }
+        
+        
+        int []answer = new int[arr.size()];
+        for(int i = 0; i < arr.size(); i++){
+            answer[i] = arr.get(i);
+        }
+
+        return answer;
+    }
+}
