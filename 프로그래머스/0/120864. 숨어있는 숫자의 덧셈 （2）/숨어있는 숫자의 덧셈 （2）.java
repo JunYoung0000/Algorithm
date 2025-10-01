db6ff4df907e6c46dacbb600049f05807c4489ec
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2025-10-01 22:10:23 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.11 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+
+        for (char ch : my_string.toCharArray()) {
+            if (Character.isDigit(ch)) { 
+                sb.append(ch);
+            } else {
+                if (sb.length() > 0) {
+                    answer += Integer.parseInt(sb.toString());
+                    sb.setLength(0);
+                }
+            }
+        }
+
+        if (sb.length() > 0) {
+            answer += Integer.parseInt(sb.toString());
+        }
+
+        return answer;
+    }
+}
