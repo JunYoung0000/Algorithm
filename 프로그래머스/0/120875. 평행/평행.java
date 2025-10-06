bd93ca35d595a48fcefa8fa07ba10516c05e06f4
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2025-10-06 13:01:59 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -4,7 +4,7 @@ class Solution {
           (double)(dots[3][1] - dots[2][1]) / (dots[3][0] - dots[2][0]))
             return 1;
         if((double)(dots[2][1] - dots[0][1]) / (dots[2][0] - dots[0][0]) ==
-        (double)(dots[3][1] - dots[1][1]) / (dots[3][0] - dots[3][0]))
+        (double)(dots[3][1] - dots[1][1]) / (dots[3][0] - dots[1][0]))
             return 1;
         
         if((double)(dots[3][1] - dots[0][1]) / (dots[3][0] - dots[0][0]) ==
