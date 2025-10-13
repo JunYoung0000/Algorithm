f813041936135388c831af44f30fe96f9e62ac72
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2025-10-13 13:53:48 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.05 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -1,13 +1,5 @@
 class Solution {
     public int solution(int chicken) {
-        int service = 0;
-
-        while (chicken >= 10) {
-            int newService = chicken / 10;
-            service += newService;
-            chicken = newService + (chicken % 10);
-        }
-
-        return service;
+        return (chicken - 1) / 9;
     }
 }
