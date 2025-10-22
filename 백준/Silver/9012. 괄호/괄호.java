13a434253fd767a36880c13b9032e9cc54816a62
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2025-10-22 13:56:45 +0900

    [Silver IV] Title: 괄호, Time: 192 ms, Memory: 18112 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < T; i++) {
+            String str = sc.nextLine();
+            System.out.println(isValid(str) ? "YES" : "NO");
+        }
+
+        sc.close();
+    }
+
+    private static boolean isValid(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : str.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
+
