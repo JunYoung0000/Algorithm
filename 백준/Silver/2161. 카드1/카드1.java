013104b376874853f02a4ed3d8061d8c385ff51e
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2025-11-19 22:17:07 +0900

    [Silver V] Title: 카드1, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.java
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -2,22 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+
         Queue<Integer> q = new LinkedList<>();
-
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 1; i <= N; i++){
+        for (int i = 1; i <= N; i++) {
             q.offer(i);
         }
 
-        while(!q.isEmpty()){
-            sb.append(q.poll());
-            sb.append(" ");
-            if(!q.isEmpty()) q.offer(q.poll());
+        StringBuilder sb = new StringBuilder();
+
+        while (q.size() > 1) {
+            // 1. 제일 위 카드를 버림
+            sb.append(q.poll()).append(" ");
+
+            // 2. 다음 카드를 뒤로 옮김
+            q.offer(q.poll());
         }
+
+        // 마지막으로 남은 카드
+        sb.append(q.poll());
 
         System.out.println(sb.toString());
     }
