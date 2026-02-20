791a85928d132f3a565a4347c8fbc003c742fd87
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2026-02-20 19:53:44 +0900

    [Silver II] Title: 섬의 개수, Time: 152 ms, Memory: 16024 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -2,23 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int[][] map;
+    static boolean[][] visited;
+    static StringTokenizer st;
+    static int w, h;
+
+    static int count = 0;
+    static int[] dy = {-1, -1, -1, 0, 1, 1, 1, 0};
+    static int[] dx = {-1, 0, 1, 1, 1, 0, -1, -1};
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-        int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
 
         while(true){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            int w = Integer.parseInt(st.nextToken());
-            int h = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
 
             if(w == 0 && h == 0) break;
 
-            int[][] map = new int[h][w];
-            boolean[][] visited = new boolean[h][w];
+            map = new int[h][w];
+            visited = new boolean[h][w];
+            count = 0;
 
             for(int i = 0; i < h; i++){
                 st = new StringTokenizer(br.readLine());
@@ -28,29 +34,11 @@ public class Main {
                 }
             }
 
-            Queue<int[]> q = new LinkedList<>();
-            int count = 0;
-
             for(int i = 0; i < h; i++){
                 for(int j = 0; j < w; j++){
                     if(!visited[i][j] && map[i][j] == 1){
-                        q.add(new int[]{i, j});
-                        visited[i][j] = true;
                         count++;
-
-                        while(!q.isEmpty()){
-                            int[] cur = q.poll();
-
-                            for(int d = 0; d < 8; d++){
-                                int nx = cur[1] + dx[d];
-                                int ny = cur[0] + dy[d];
-
-                                if(nx >= 0 && ny >= 0 && nx < w && ny < h && !visited[ny][nx] && map[ny][nx] == 1){
-                                    q.add(new int[]{ny, nx});
-                                    visited[ny][nx] = true;
-                                }
-                            }
-                        }
+                        dfs(i, j);
                     }
                 }
             }
@@ -59,5 +47,20 @@ public class Main {
         }
 
         System.out.println(sb);
+    }
+
+    static void dfs(int y, int x){
+        visited[y][x] = true;
+
+        for(int d = 0; d < 8; d++){
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+
+            if(nx >= 0 && ny >= 0 && nx < w && ny < h){
+                if(!visited[ny][nx] && map[ny][nx] == 1){
+                    dfs(ny, nx);
+                }
+            }
+        }
     }
 }
