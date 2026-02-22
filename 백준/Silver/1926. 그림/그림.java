9b19d69842f13d8eba95c22376140614653ff608
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2026-02-22 12:00:47 +0900

    [Silver I] Title: 그림, Time: 284 ms, Memory: 51212 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -2,61 +2,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static int[][] map;
+    static boolean[][] visited;
+    static int maxExtent = 0, count, extent;
+    static int n, m;
+
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        int[][] map = new int[n][m];
-        boolean[][] visited = new boolean[n][m];
+        map = new int[n][m];
+        visited = new boolean[n][m];
 
         for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-
             for(int j = 0; j < m; j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
-        Queue<int[]> q = new LinkedList<>();
-        int count = 0;
-        int maxArea = 0;
-
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                int area = 0;
-
                 if(!visited[i][j] && map[i][j] == 1){
-                    q.add(new int[]{i, j});
-                    visited[i][j] = true;
                     count++;
-
-                    while(!q.isEmpty()){
-                        int[] cur = q.poll();
-                        area++;
-
-                        for(int d = 0; d < 4; d++){
-                            int nx = cur[1] + dx[d];
-                            int ny = cur[0] + dy[d];
-
-                            if(nx >= 0 && ny >= 0 && nx < m && ny < n && !visited[ny][nx] && map[ny][nx] == 1){
-                                q.add(new int[]{ny, nx});
-                                visited[ny][nx] = true;
-                            }
-                        }
-                    }
-
-                    if(maxArea < area) maxArea = area;
+                    extent = 0;
+                    dfs(i, j);
+                    if(maxExtent < extent) maxExtent = extent;
                 }
             }
         }
 
         System.out.println(count);
-        System.out.println(maxArea);
+        System.out.println(maxExtent);
+    }
+
+    static void dfs(int y, int x){
+        visited[y][x] = true;
+        extent++;
+
+        for(int d = 0; d < 4; d++){
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+
+            if(nx >= 0 && ny >= 0 && nx < m && ny < n){
+                if(!visited[ny][nx] && map[ny][nx] == 1){
+                    dfs(ny, nx);
+                }
+            }
+        }
     }
 }
