b659ff1061285fa6542bd9ae87a22959358a65dd
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2026-03-03 20:43:52 +0900

    [Silver V] Title: 방 번호, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -2,13 +2,10 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String input = br.readLine();
-        List<Integer> list = new ArrayList<>();
 
         int[] arr = new int[10];
         int num;
