2c2c892c32b3799e76dbde8a89e60a678596f556
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2026-03-18 22:25:36 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 180 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        String[] parts = input.split("-");
+
+        int result = 0;
+
+        result += sum(parts[0]);
+
+        for (int i = 1; i < parts.length; i++) {
+            result -= sum(parts[i]);
+        }
+
+        System.out.println(result);
+    }
+
+    static int sum(String str) {
+        String[] nums = str.split("\\+");
+        int total = 0;
+
+        for (String num : nums) {
+            total += Integer.parseInt(num);
+        }
+
+        return total;
+    }
+}
