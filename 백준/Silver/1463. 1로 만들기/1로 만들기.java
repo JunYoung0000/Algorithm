a25eca11eb2f323b12a6e64605b02113daeaadc9
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2026-03-23 21:56:47 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18160 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[X + 1];
+
+        dp[1] = 0; // 1은 이미 1이니까 연산 0번
+
+        for (int i = 2; i <= X; i++) {
+
+            // 1 빼는 경우
+            dp[i] = dp[i - 1] + 1;
+
+            // 2로 나누어 떨어지는 경우
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+
+            // 3으로 나누어 떨어지는 경우
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+        }
+
+        System.out.println(dp[X]);
+    }
+}
