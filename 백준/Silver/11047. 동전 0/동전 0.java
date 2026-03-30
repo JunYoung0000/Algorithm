47b5e5174170d412994b6c563922535da884d1b9
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2026-03-30 23:18:18 +0900

    [Silver IV] Title: 동전 0, Time: 112 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,27 +1,29 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int[] coins = new int[N];
+        int[] arr = new int[N];
 
-        for (int i = 0; i < N; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
         int count = 0;
-        for (int i = N - 1; i >= 0; i--) {
-            count += K / coins[i];
-            K %= coins[i];
+
+        for(int i = N - 1; i >= 0; i--) {
+            count += K / arr[i];
+            K %= arr[i];
         }
 
         System.out.println(count);
     }
 }
-
