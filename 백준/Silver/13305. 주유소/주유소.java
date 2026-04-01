06f383d5c4284928c47cb56020deafb02e1d4a63
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2026-04-01 21:31:24 +0900

    [Silver III] Title: 주유소, Time: 368 ms, Memory: 36512 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        long[] dist = new long[N - 1];
+        long[] price = new long[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N - 1; i++) {
+            dist[i] = Long.parseLong(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            price[i] = Long.parseLong(st.nextToken());
+        }
+
+        long minPrice = price[0];
+        long total = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            minPrice = Math.min(minPrice, price[i]);
+
+            total += minPrice * dist[i];
+        }
+
+        System.out.println(total);
+    }
+}
