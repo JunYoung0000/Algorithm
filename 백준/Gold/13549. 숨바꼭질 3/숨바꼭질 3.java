18bdb9b796ce5f79d9f0690255077ce2921f1380
JunYoung <141308805+JunYoung0000@users.noreply.github.com>
2026-04-05 01:47:27 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 132 ms, Memory: 16152 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
     static final int MAX = 100000;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -14,39 +14,34 @@ public class Main {
         int[] dist = new int[MAX + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
 
-        if (N >= K) {
-            System.out.println(N - K);
-            return;
-        }
-
         Deque<Integer> dq = new ArrayDeque<>();
-
         dq.offer(N);
         dist[N] = 0;
-        while (!dq.isEmpty()) {
-            int now = dq.poll();
 
-            if (now == K) {
-                System.out.println(dist[now]);
+        while(!dq.isEmpty()){
+            int cur = dq.poll();
+
+            if (cur == K){
+                System.out.println(dist[cur]);
                 return;
             }
 
-            int teleport = now * 2;
-            if(teleport >= 0 && teleport <= MAX && dist[teleport] > dist[now]) {
-                dist[teleport] = dist[now];
-                dq.offerFirst(teleport);
+            int next = cur * 2;
+            if (next <= MAX && dist[next] > dist[cur]){
+                dist[next] = dist[cur];
+                dq.addFirst(next);
             }
 
-            int plus = now + 1;
-            if(plus <= MAX && dist[plus] > dist[now] + 1) {
-                dist[plus] = dist[now] + 1;
-                dq.offerLast(plus);
+            next = cur - 1;
+            if (next >= 0 && dist[next] > dist[cur] + 1){
+                dist[next] = dist[cur] + 1;
+                dq.addLast(next);
             }
 
-            int minus = now - 1;
-            if(minus >= 0 && dist[minus] > dist[now] + 1) {
-                dist[minus] = dist[now] + 1;
-                dq.offerLast(minus);
+            next = cur + 1;
+            if (next <= MAX && dist[next] > dist[cur] + 1){
+                dist[next] = dist[cur] + 1;
+                dq.addLast(next);
             }
         }
     }
